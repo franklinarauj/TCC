520f354dc65e01258f1aa2c3a35de209b20ee14c
Murilo Kronbauer <murilokronbauer@gmail.com>
2021-10-25 22:02:58 -0300

    Modificando estrutura e importações

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { TermsComponent } from './register/terms/terms.component';
-import { CadastroComponent } from './register/cadastro/cadastro.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { TermsComponent } from './pages/register/terms/terms.component';
+import { CadastroComponent } from './pages/register/cadastro/cadastro.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+// PAGES
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { TermsComponent } from './pages/register/terms/terms.component';
+import { CadastroComponent } from './pages/register/cadastro/cadastro.component';
+
 // COMPONENTS
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './login/login.component';
-import { TermsComponent } from './register/terms/terms.component';
-import { CadastroComponent } from './register/cadastro/cadastro.component';
+
 //IMPORTS @ANGULAR
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
